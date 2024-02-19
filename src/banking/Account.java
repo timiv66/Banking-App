082d9169bd8760149bd947644cc10593388710e1
Timi Verissimo <timiv9@gmail.com>
2024-02-18 21:52:21 -0500

    Creation of the User and Account class

---
+++ src/banking/Account.java
@@ -0,0 +1,30 @@
+package banking;
+
+public class Account {
+	private String type;
+	private String name;
+	private double total;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+	Account(){
+		
+	}
+}
---
+++ src/banking/User.java
@@ -0,0 +1,31 @@
+package banking;
+
+public class User {
+	private String name;
+	private int pin;
+	private Account account;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	public Account getAccount() {
+		return account;
+	}
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	
+	User(){
+		
+	}
+	
+}
